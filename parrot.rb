230e872d84c09cf8938882b37bfb1cee745de692
Michael Fox <michael.c.fox.ii@gmail.com>
2019-05-09 03:08:58 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,11 +1,15 @@
 def parrot(arg1="Squawk!",arg2="Pretty Bird!")
   "#{arg1}"
-  end
-  
   if "#{arg1}"
     puts "Squawk!"
   else
     puts "Pretty Bird!"
   end
+  
+  #if "#{arg1}"
+    #puts "Squawk!"
+  #else
+   # puts "Pretty Bird!"
+  #end
   # Create method `parrot` that outputs a given phrase and
 # returns the phrase
