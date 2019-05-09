ddbe0ef332a629317aaf9a3dbf51392d165e64ad
Michael Fox <michael.c.fox.ii@gmail.com>
2019-05-09 03:53:49 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,15 @@
-# Create method `parrot` that outputs a given phrase and
+def parrot(arg1="Squawk!",arg2="Pretty Bird!")
+  puts"#{arg1}"
+  return"#{arg1}"
+  end
+  
+#def parrot(arg1="Squawk!",arg2="Pretty Bird!")
+ # puts"#{arg1}"
+  #end
+# if "#{arg1}"
+#    puts "Squawk!"
+#  else
+#   puts "Pretty Bird!"
+#  end
+  # Create method `parrot` that outputs a given phrase and
 # returns the phrase
